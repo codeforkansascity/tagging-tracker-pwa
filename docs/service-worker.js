f6f668543f787e59efafc7421cfb912d6d0884ba
Jacob Cunningham <jcun4128@gmail.com>
2020-07-25 15:57:07 -0500

    looks good on PWA Safari

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef3690b8c2a0c3ef674d98dfb71216fb.js"
+  "/precache-manifest.a52680ef641524eaf221222e1a6957d7.js"
 );
 
 self.addEventListener('message', (event) => {
